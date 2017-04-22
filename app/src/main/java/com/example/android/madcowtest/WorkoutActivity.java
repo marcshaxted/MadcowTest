8d1c3b5e30b62e1007871e0e25dbe823b3450c7a
mds1978 <mdshaxted@gmail.com>
2017-04-22 16:03:27 +0100

    Removed redundant code from WorkoutActivity

--- app/src/main/java/com/example/android/madcowtest/WorkoutActivity.java
+++ app/src/main/java/com/example/android/madcowtest/WorkoutActivity.java
@@ -2,11 +2,6 @@ package com.example.android.madcowtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class WorkoutActivity extends AppCompatActivity {
 
@@ -15,19 +10,5 @@ public class WorkoutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
-        TextView headerTextView = (TextView) this.findViewById(R.id.header).findViewById(R.id.header_title_text_view);
-        headerTextView.setText("Workout A");
-
-        TextView dateTextView = (TextView) this.findViewById(R.id.header).findViewById(R.id.header_date_text_view);
-        dateTextView.setText("25th December 2017");
-
-        LinearLayout exercises = (LinearLayout) this.findViewById(R.id.exercises);
-
-        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
-        View exerciseLayout = inflater.inflate(R.layout.exercise, (ViewGroup) findViewById(R.id.exercise));
-
-        for (int i = 0; i < 2; i++) {
-            exercises.addView(exerciseLayout);
-        }
     }
 }
