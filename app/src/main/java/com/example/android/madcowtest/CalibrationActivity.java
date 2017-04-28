65cf33c3bfdb2258c148dedd8e17ad9adb954576
mds1978 <mdshaxted@gmail.com>
2017-04-28 19:50:22 +0100

    Added more functionality to calibrations

--- app/src/main/java/com/example/android/madcowtest/CalibrationActivity.java
+++ app/src/main/java/com/example/android/madcowtest/CalibrationActivity.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class CalibrationActivity extends AppCompatActivity {
+
+    private final double ROUNDING = 1.0;
+
+    private LayoutInflater mInflater;
 
     private TextWatcher mTextWatcher = new TextWatcher() {
         @Override
@@ -21,9 +28,10 @@ public class CalibrationActivity extends AppCompatActivity {
         @Override
         public void afterTextChanged(Editable s) {
 
-            TextView result = (TextView) findViewById(R.id.squats_result);
-            String reps = ((EditText) findViewById(R.id.squats_reps)).getText().toString();
-            String weight = ((EditText) findViewById(R.id.squats_weight)).getText().toString();
+            //This needs to be more specific
+            TextView result = (TextView) findViewById(R.id.result);
+            String reps = ((EditText) findViewById(R.id.reps)).getText().toString();
+            String weight = ((EditText) findViewById(R.id.weight)).getText().toString();
             int repsValue = 0;
             double weightValue = 0;
             try {
@@ -45,26 +53,74 @@ public class CalibrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibration);
-        setupEventListeners();
+
+        mInflater = LayoutInflater.from(this);
+        ((LinearLayout) this.findViewById(R.id.calibrations)).removeAllViews();
+
+        String[] exercises = new String[]{"squat 1rm", "bench 1rm", "deadlift 1rm", "press 1rm"};
+
+        for (String s : exercises) {
+
+            LinearLayout calib = (LinearLayout) mInflater.inflate(R.layout.calibration, null);
+            ((TextView) calib.findViewById(R.id.name)).setText(s);
+            ((LinearLayout) this.findViewById(R.id.calibrations)).addView(calib);
+            setupEventListeners(calib);
+        }
     }
 
-    private void setupEventListeners() {
+    private void setupEventListeners(LinearLayout calib) {
 
-        EditText repsInput = (EditText) findViewById(R.id.squats_reps);
-        repsInput.addTextChangedListener(mTextWatcher);
+        EditText repsInput = (EditText) calib.findViewById(R.id.reps);
+        repsInput.addTextChangedListener(new GenericTextWatcher(calib));
 
-        EditText weightInput = (EditText) findViewById(R.id.squats_weight);
-        weightInput.addTextChangedListener(mTextWatcher);
-
+        EditText weightInput = (EditText) calib.findViewById(R.id.weight);
+        weightInput.addTextChangedListener(new GenericTextWatcher(calib));
     }
 
     private double calcOneRepMax(int reps, double weight) {
-        double retVal = weight;
-        double foo = (double) reps / 30;
-        foo = foo + 1;
-        retVal = retVal * foo;
+//        double retVal = weight;
+//        double foo = (double) reps / 30;
+//        foo = foo + 1;
+//        retVal = retVal * foo;
+        return weight / (1.0278 - (0.0287 * (double) reps));
+    }
 
+    private class GenericTextWatcher implements TextWatcher {
 
-        return retVal;
+        private View mView;
+
+        private GenericTextWatcher(View view) {
+            mView = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable s) {
+
+            //This needs to be more specific
+            TextView result = (TextView) mView.findViewById(R.id.result);
+            String reps = ((EditText) mView.findViewById(R.id.reps)).getText().toString();
+            String weight = ((EditText) mView.findViewById(R.id.weight)).getText().toString();
+
+            int repsValue = 0;
+            double weightValue = 0;
+            try {
+                repsValue = Integer.parseInt(reps);
+                weightValue = Double.parseDouble(weight);
+            } catch (Exception e) {
+            }
+
+            double res = calcOneRepMax(repsValue, weightValue);
+//            res = (int) Math.round(res / ROUNDING);
+//            res = res * ROUNDING;
+
+            if (res != 0) {
+                result.setText(String.valueOf((int) res));
+            }
+        }
     }
 }
